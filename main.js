64f9e71ad487ab15529dee203a13b250ae7c4437
saikksub <saikksub@gmail.com>
2018-02-12 23:52:20 +0530

    Add keyboard user interface
    Should complete display

--- main.js
+++ main.js
@@ -7,8 +7,16 @@ const url = require('url')
 let win
 
 function createWindow () {
+
+  let width = 300, height = 450;
+
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    width: width, height: height,
+    minWidth: width, minHeight: height,
+    maxWidth: width, maxHeight: height,
+    titleBarStyle: 'hidden'
+  })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -18,7 +26,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
